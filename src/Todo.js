942a71afabb8d70a7adb68d5c957cfccc6792ca1
Shazammon <ammonorgill@gmail.com>
2023-01-31 18:51:35 -0300

    added checkbox

--- src/Todo.js
+++ src/Todo.js
@@ -4,9 +4,10 @@ export default function Todo( {todo} ) {
   return (
     <div>
         <label>
-            
+            <input type="checkbox" checked={todo.complete}
+            />
+            {todo.name}
         </label>
-      {todo.name}
     </div>
   )
 }
