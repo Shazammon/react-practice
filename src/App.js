3744c6bcdf6e2b7f474e28aaa542004fcbd6b02a
Shazammon <ammonorgill@gmail.com>
2023-01-31 17:33:42 -0300

    added buttons

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <>
       <TodoList />
       <input type="text"/>
+      <button>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left to do</div>
     </>
   )
 }
