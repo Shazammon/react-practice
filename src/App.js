f694ae96e74dc279b86bd72d0fed43cbc8f6911d
Shazammon <ammonorgill@gmail.com>
2023-01-31 17:25:26 -0300

    added initial todolist component code and input box

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import TodoList from './TodoList';
 
 function App() {
   return (
-    <TodoList />
+    <>
+      <TodoList />
+      <input type="text"/>
+    </>
   )
 }
 
