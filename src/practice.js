e6be71e84791b9e4a8e06225b52ab0b3bf0b6f33
Shazammon <ammonorgill@gmail.com>
2023-02-01 14:36:38 -0300

    wrote function to practice object destructuring

--- src/practice.js
+++ src/practice.js
@@ -11,3 +11,14 @@ console.log(a)
 console.log(c)
 console.log(rest)
 console.log(newArray)
+
+function sumAndMultiply(a, b) {
+    return [a+b, a*b]
+}
+
+const array = sumAndMultiply(2,3)
+const [sum, multiply] = sumAndMultiply(2,3)
+
+
+console.log(array)
+console.log(sum, multiply)
