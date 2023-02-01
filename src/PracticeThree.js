4d48041b07ad6df0d524328606583c37b81902dc
Shazammon <ammonorgill@gmail.com>
2023-02-01 18:11:21 -0300

    added additional ref variable

--- src/PracticeThree.js
+++ src/PracticeThree.js
@@ -2,9 +2,10 @@ import React, { useRef } from 'react'
 
 export default function PracticeThree() {
   const yearRef = useRef()
+  const monthRef = useRef()
 
   function handleChange(e) {
-    
+
   }
 
   return (
@@ -13,12 +14,14 @@ export default function PracticeThree() {
           type='text'
           maxLength={2}
           placeholder='MM'
+          ref={monthRef}
         ></input>
 
         <input 
           type='text'
           maxLength={4}
           placeholder='YYYY'
+          ref={yearRef}
         ></input>
     </>
   )
