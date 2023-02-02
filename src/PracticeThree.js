e9c4d667b5059b871c57ca7f27c7dcd623ed1fa8
Shazammon <ammonorgill@gmail.com>
2023-02-02 13:17:06 -0300

    playing around with tailwind formatting for input box

--- src/PracticeThree.js
+++ src/PracticeThree.js
@@ -19,6 +19,18 @@ export default function PracticeThree() {
 
   return (
     <>
+      <div class="mb-6">
+    <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Month</label>
+    <input type="text" 
+            name="field-1"
+            maxLength={2}
+            placeholder='MM'
+            ref={monthRef}
+            onChange={handleChange}
+    id="large-input" 
+    class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+</div>
+      {/* <label>Month
         <input 
           type='text'
           name="field-1"
@@ -26,7 +38,9 @@ export default function PracticeThree() {
           placeholder='MM'
           ref={monthRef}
           onChange={handleChange}
-        ></input>
+        ></input> */}
+
+      {/* </label> */}
 
         <input 
           type='text'
